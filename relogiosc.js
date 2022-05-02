63909aafd454b6c38e4829c97ece892ebea8b3bd
tmazleo <leotomazb@icloud.com>
2022-05-02 19:35:17 -0300

    relogio funcionando

---
+++ relogiosc.js
@@ -0,0 +1,19 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let presente = new Date();
+    let hr = presente.getHours();
+    let min = presente.getMinutes();
+    let sec = presente.getSeconds();
+    
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+    if (sec < 10) sec = '0' + sec;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sec;
+})
+
